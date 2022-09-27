ad7db5c1fcfad7c1bee5684039638b005811996d
stecala <ste.cala123@gmail.com>
2022-09-27 16:05:03 +0200

    feat: :sparkles: app.vue now shows all the descriptions of posts

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-         
+        <h1>Posts:</h1>
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                {{ post.description }}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -8,14 +13,26 @@
 import axios from 'axios';
 export default {
     data: function(){
-    
+        return{
+            posts: [],
+
+        }
     },
     methods: {
-    
-    
+    //! creo una funzione per effetuare la chiamata API
+        ApiCallPosts(){
+            axios.get('/api/posts')
+            .then((result)=>{
+                this.posts=result.data.results.data
+                console.log(result.data.results)
+            })
+            .catch((error)=>{
+                console.error(error)
+            })
+        }
     },
     created (){
-
+        this.ApiCallPosts()
     }
 }
 </script>
