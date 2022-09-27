02abae7e52ea4043ecf73bf5886c65d46ad5cb00
stecala <ste.cala123@gmail.com>
2022-09-27 15:42:43 +0200

    feat: :sparkles: installed axios and working on the first component

---
+++ resources/js/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+         
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data: function(){
+    
+    },
+    methods: {
+    
+    
+    },
+    created (){
+
+    }
+}
+</script>
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Route::get('/', function () {
+    return view('guest.home');
+}); */
 
 Auth::routes();
 
